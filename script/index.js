310c3f8f0795a55cc1951d0807a3c3e75cfa129d
joaopedrobagli <joaobagli.pedro1506@gmail.com>
2025-05-08 21:16:03 -0300

    atual

--- script/index.js
+++ script/index.js
@@ -2,11 +2,20 @@ const navbar = document.querySelector('.navbar');
 const mobileNavbar = document.querySelector('.navbar__mobile');
 const button = document.querySelector('.burguer');
 
-button.addEventListener('click', function () {
-  mobileNavbar.classList.toggle('active');
-});
+if (button) {
+  button.addEventListener('click', function () {
+    mobileNavbar.classList.toggle('active');
+  });
+}
 
 window.addEventListener('scroll', function () {
-  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
-  return navbar.classList.remove('active');
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('active');
+  } else {
+    navbar.classList.remove('active');
+  }
 });
+
+
+
+
